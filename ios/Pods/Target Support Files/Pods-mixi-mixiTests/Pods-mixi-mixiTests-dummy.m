d945660db4c6085601aeacf3d1e83de3fd260042
Ngo Quang Trieu <ngoquangtrieu@V000061s-Mac-mini.local>
2020-06-19 14:52:34 +0700

    init project

---
+++ ios/Pods/Target Support Files/Pods-mixi-mixiTests/Pods-mixi-mixiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mixi_mixiTests : NSObject
+@end
+@implementation PodsDummy_Pods_mixi_mixiTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-mixi-tvOSTests/Pods-mixi-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mixi_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_mixi_tvOSTests
+@end
